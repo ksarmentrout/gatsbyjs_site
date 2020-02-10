c9f8a8b895c07440ec151f3fa56319dc6955d646
ksarmentrout <k.armentrout11@gmail.com>
2020-02-09 19:18:56 -0500

    Added favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,18 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     // `gatsby-plugin-dark-mode`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `keaton_armentrout_site`,
+        short_name: `ksarmentrout`,
+        start_url: `/`,
+        // background_color: `#663399`,
+        // theme_color: `#663399`,
+        // display: `minimal-ui`,
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
@@ -50,17 +62,6 @@ module.exports = {
         mergeCachingHeaders: true, // boolean to turn off the default caching headers
         transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
         generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
