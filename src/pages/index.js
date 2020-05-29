f07b9e0065662f5fdba0c5b2e366157b42b0aed2
ksarmentrout <k.armentrout11@gmail.com>
2020-05-29 13:40:20 -0400

    implemented error boundary

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
 
+import ErrorBoundary from "../components/ErrorBoundary"
 import LandingBio from "../components/landing-bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <LandingBio />
-  </Layout>
+  <ErrorBoundary>
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <LandingBio />
+    </Layout>
+  </ErrorBoundary>
 );
 
 export default IndexPage
