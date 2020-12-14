2cc18cf6e5c2ca1d02c9557e4f9ced83b9332a14
ksarmentrout <k.armentrout11@gmail.com>
2020-12-13 16:35:57 -0800

    Added gatsby video embeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,23 @@ module.exports = {
         plugins: [
           `gatsby-remark-reading-time`,
           {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              containerClass: 'embedVideo-container', //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+              urlOverrides: [
+                {
+                  id: 'youtube',
+                  embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
+                }
+              ] //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+            }
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               aliases:{sh: "bash", js:"javascript"},
